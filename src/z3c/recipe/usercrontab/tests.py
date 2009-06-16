8fd369d03ba326bb1588279e03abb4275b9f4921
reinout <reinout@vanrees.org>
2009-06-16 12:57:40 +0000

    - Removed essentially-unused complete environment variable handling. - Adding our entries with descriptive comments now: it includes the buildout   file and the part name.

--- src/z3c/recipe/usercrontab/tests.py
+++ src/z3c/recipe/usercrontab/tests.py
@@ -16,16 +16,16 @@ from zope.testing import doctest, renormalizing
 
 from z3c.recipe.usercrontab import UserCrontabManager
 
-usercrontab = UserCrontabManager()
+usercrontab = UserCrontabManager(identifier='test')
 
 def setUp(test):
     zc.buildout.testing.buildoutSetUp(test)
-    usercrontab.read_crontab()
+    usercrontab.read_crontab() # Store current user's real crontab.
     zc.buildout.testing.install_develop('z3c.recipe.usercrontab', test)
 
 def tearDown(test):
     zc.buildout.testing.buildoutTearDown(test)
-    usercrontab.write_crontab()
+    usercrontab.write_crontab() # Restore current user's real crontab.
 
 def test_suite():
     return unittest.TestSuite(doctest.DocFileSuite('README.txt', setUp=setUp,
